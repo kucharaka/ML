8e23bb2d100a368091424ceb8f26d65c8b3622a5
kucharaka <53340528+kucharaka@users.noreply.github.com>
2020-04-26 15:08:16 +0300

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,19 +1,19 @@
-import random as rnd
-import matplotlib.pyplot as plt
+from random import randint
+from matplotlib.pyplot import plot, show
 
 s, s1, k, b = 0, 0, 1, 1
 y1 = []
 x = [l for l in range(16, 100)]
-y = [108 + rnd.randint(-25, 25) + 2 * l for l in x]
+y = [randint(105, 115) + randint(-25, 25) + 2 * l for l in x]
 for i in range(60000):
     for j in range(0, 84):
-        s += ((b + k * x[j]) - y[j]) * x[j]
-        s1 += ((b + k * x[j]) - y[j])
-    k -= (0.0006 * 0.01 * s)
-    b -= (0.0006 * 0.01 * s1)
+        q = (b + k * x[j]) - y[j]
+        s += q * x[j]
+        s1 += q
+    k -= 0.0006 * 0.01 * s
+    b -= 0.0006 * 0.01 * s1
     s, s1 = 0, 0
 for i in x:
     y1.append(b + k * i)
-
-plt.plot(x, y, 'ro', x, y1)
-plt.show()
+plot(x, y, 'ro', x, y1)
+show()
